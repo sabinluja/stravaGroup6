12d1084cda266d982e6bff47a29c85c281cd66c2
sabinluja <sabin.luja@opendeusto.es>
2023-11-10 11:13:45 +0100

    DTO + ASSEMBLER

--- stravaServer/src/es/deusto/ingenieria/sd/auctions/server/data/dto/UserAssembler.java
+++ stravaServer/src/es/deusto/ingenieria/sd/auctions/server/data/dto/UserAssembler.java
@@ -2,26 +2,22 @@ package es.deusto.ingenieria.sd.auctions.server.data.dto;
 
 import es.deusto.ingenieria.sd.auctions.server.data.domain.User;
 
-//This class is part of the DTO pattern. It also implements Singleton Pattern.
 public class UserAssembler {
-	private static UserAssembler instance;
+    private static UserAssembler instance;
 
-	private UserAssembler() { }
-	
-	public static UserAssembler getInstance() {
-		if (instance == null) {
-			instance = new UserAssembler();
-		}
+    private UserAssembler() {}
 
-		return instance;
-	}
+    public static UserAssembler getInstance() {
+        if (instance == null) {
+            instance = new UserAssembler();
+        }
+        return instance;
+    }
 
-	public UserDTO userToDTO(User user) {
-		UserDTO dto = new UserDTO();
-		
-		dto.setEmail(user.getEmail());
-		dto.setNickname(user.getNickname());
-		
-		return dto;
-	}
+    public UserDTO userToDTO(User user) {
+        // Conversion logic from User to UserDTO
+        // Implement conversion here
+    	
+    	return null;
+    }
 }
