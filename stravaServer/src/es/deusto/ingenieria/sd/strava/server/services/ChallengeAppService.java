3a98be7fc1646c1b80c4bdf83e5a71df8bbe1c0f
sabinluja <sabin.luja@opendeusto.es>
2023-11-10 20:14:30 +0100

    challenge

--- stravaServer/src/es/deusto/ingenieria/sd/strava/server/services/ChallengeAppService.java
+++ stravaServer/src/es/deusto/ingenieria/sd/strava/server/services/ChallengeAppService.java
@@ -1,10 +1,36 @@
 package es.deusto.ingenieria.sd.strava.server.services;
 
+import java.util.List;
+
+import es.deusto.ingenieria.sd.strava.server.data.domain.Challenge;
+import es.deusto.ingenieria.sd.strava.server.data.domain.User;
+
 public class ChallengeAppService {
 	
-	public ChallengeAppService() {
-		this.initilizeData();
-	}
+	public ChallengeAppService() {}
 
-	private void initilizeData() {}
+	@SuppressWarnings("unused")
+	private boolean createChallengue(User user, String name, String startDate, String endDate, float targetDistance, 
+			long targetTime, String sport) {
+		
+		return true;
+	}
+	
+	@SuppressWarnings("unused")
+	private boolean acceptChallengue(User user, String challengeName) {
+		
+		return true;
+	}
+	
+	@SuppressWarnings("unused")
+	private List<Challenge> getActiveChallengues(User user, String date) {
+		
+		return null;
+	}
+	
+	@SuppressWarnings("unused")
+	private List<Challenge> getAcceptedChallengues(User user) {
+
+		return null;
+	}
 }
