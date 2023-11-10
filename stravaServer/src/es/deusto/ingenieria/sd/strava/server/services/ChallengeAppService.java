3916b62071e1938a90b7ee6826556b5d6dcc339c
sabinluja <sabin.luja@opendeusto.es>
2023-11-10 18:16:36 +0100

    rename

--- stravaServer/src/es/deusto/ingenieria/sd/strava/server/services/ChallengeAppService.java
+++ stravaServer/src/es/deusto/ingenieria/sd/strava/server/services/ChallengeAppService.java
@@ -1,5 +1,10 @@
 package es.deusto.ingenieria.sd.strava.server.services;
 
 public class ChallengeAppService {
+	
+	public ChallengeAppService() {
+		this.initilizeData();
+	}
 
+	private void initilizeData() {}
 }
