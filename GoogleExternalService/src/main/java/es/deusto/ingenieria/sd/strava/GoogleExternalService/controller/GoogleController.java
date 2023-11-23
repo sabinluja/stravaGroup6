35311364dd049b8093dcc09cd7a5562f78b39de5
aitormolero <aitor.molero@opendeusto.es>
2023-11-23 23:58:50 +0100

    google service and controller

--- GoogleExternalService/src/main/java/es/deusto/ingenieria/sd/strava/GoogleExternalService/controller/GoogleController.java
+++ GoogleExternalService/src/main/java/es/deusto/ingenieria/sd/strava/GoogleExternalService/controller/GoogleController.java
@@ -1,27 +1,33 @@
 package es.deusto.ingenieria.sd.strava.GoogleExternalService.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import es.deusto.ingenieria.sd.strava.GoogleExternalService.service.GoogleService;
 
 public class GoogleController {
 
-	@GetMapping("/user/register/{email}/{password}")
-	public boolean register(@PathVariable String email, @PathVariable String password) {
-		
-		
-		return false;
-	}
+    private static final Logger log= LoggerFactory.getLogger(GoogleController.class);
 
-	@GetMapping("/validate/password/{email}/{password}")
-	public boolean validatePassword(@PathVariable String email, @PathVariable String password) {
-		
-		
-		return false;
-	}
+    private GoogleService googleService;
 
-	@GetMapping("/validate/email/{email}")
-	public boolean validateEmail(@PathVariable String email) {
-		
-		
-		return false;
-	}
-} 
+
+    @GetMapping("/user/register/{email}/{password}")
+    public boolean register(@PathVariable String email, @PathVariable String password) {
+        log.info("Registering a user");
+        return this.googleService.register(email, password);
+    }
+
+    @GetMapping("/validate/password/{email}/{password}")
+    public boolean validatePassword(@PathVariable String email, @PathVariable String password) {
+        log.info("Validating password");
+        return this.googleService.validatePassword(email, password);
+    }
+
+    @GetMapping("/validate/email/{email}")
+    public boolean validateEmail(@PathVariable String email) {
+    	log.info("Validating email");
+        return this.googleService.validateEmail(email);
+    }
+}
--- GoogleExternalService/src/main/java/es/deusto/ingenieria/sd/strava/GoogleExternalService/service/GoogleService.java
+++ GoogleExternalService/src/main/java/es/deusto/ingenieria/sd/strava/GoogleExternalService/service/GoogleService.java
@@ -1,28 +1,40 @@
 package es.deusto.ingenieria.sd.strava.GoogleExternalService.service;
 
+import java.util.HashMap;
+
 import es.deusto.ingenieria.sd.strava.server.gateways.IProviderGateway;
 
 public class GoogleService implements IProviderGateway {
 
-	@Override
-	public boolean register(String email, String password) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    private HashMap<String, String> registeredUser = new HashMap<>(); //key:email value:password
 
-	@Override
-	public boolean validatePassword(String email, String password) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public boolean register(String email, String password) {
+        registeredUser.put(email, password);
+        return true;
+    }
 
-	@Override
-	public boolean validateEmail(String email) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public boolean validatePassword(String email, String password) {
+        if (!registeredUser.containsKey(email)) { 
+            return false;
+        } else {
+            String registeredPassword = registeredUser.get(email);
+            if (password.equals(registeredPassword)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 
-	
-
+    @Override
+    public boolean validateEmail(String email) { //if the email is already registered return false
+        if(registeredUser.containsKey(email)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
 }
