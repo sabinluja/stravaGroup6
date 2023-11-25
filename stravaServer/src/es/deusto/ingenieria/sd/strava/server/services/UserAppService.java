6a97fcc94694529e2a1c53f6a196c8cbe7e86b68
sabinluja <sabin.luja@opendeusto.es>
2023-11-25 21:50:05 +0100

    window

--- stravaServer/src/es/deusto/ingenieria/sd/strava/server/services/UserAppService.java
+++ stravaServer/src/es/deusto/ingenieria/sd/strava/server/services/UserAppService.java
@@ -7,21 +7,18 @@ import es.deusto.ingenieria.sd.strava.server.data.domain.Challenge;
 import es.deusto.ingenieria.sd.strava.server.data.domain.Session;
 import es.deusto.ingenieria.sd.strava.server.data.domain.User;
 import es.deusto.ingenieria.sd.strava.server.gateways.GatewayFactory;
-import es.deusto.ingenieria.sd.strava.server.gateways.IProviderGateway; 
+import es.deusto.ingenieria.sd.strava.server.gateways.IProviderGateway;
 
 public class UserAppService {
-	
-	User user1;
-	private List<User> registeredUsers = new ArrayList<User>();
-	
-	public UserAppService() {
-		initialize();
-	}
-	
-	public void initialize() {
+    User user1;
+    private List<User> registeredUsers = new ArrayList<User>();
 
-		// CHALLENGES
-		
+    public UserAppService() {
+        initialize();
+    }
+
+    public void initialize() {
+        // CHALLENGES
         Challenge challenge1 = new Challenge();
         challenge1.setName("Challenge 1");
         challenge1.setStartDate("2023-01-01");
@@ -37,7 +34,7 @@ public class UserAppService {
         challenge2.setTargetTime(1800); // 30 minutes
         challenge2.setTargetDistance(5.0f); // 5 kilometers
         challenge2.setSports("Cycling");
-	
+
         Challenge challenge3 = new Challenge();
         challenge3.setName("Challenge 3");
         challenge3.setStartDate("2023-03-01");
@@ -62,9 +59,7 @@ public class UserAppService {
         challenge5.setTargetDistance(20.0f); // 20 kilometers
         challenge5.setSports("Cycling");
 
-        
-        // SESIONS 
-        
+        // SESSIONS
         Session session1 = new Session();
         session1.setTitle("Session 1");
         session1.setSports("Running");
@@ -80,7 +75,7 @@ public class UserAppService {
         session2.setStartDate("2023-02-10");
         session2.setStartTime(System.currentTimeMillis());
         session2.setDuration(3600); // 1 hour
-        
+
         Session session3 = new Session();
         session3.setTitle("Session 3");
         session3.setSports("Running");
@@ -105,147 +100,114 @@ public class UserAppService {
         session5.setStartTime(System.currentTimeMillis());
         session5.setDuration(7200); // 2 hours
 
-        
         // USERS
-        
         user1 = new User();
-        user1.setName("Sabin");
-        user1.setEmail("sabin.luja@opendeusto.es");
-        user1.setBirthDate("2003-01-01");
-        user1.setProvider("Google");
+        user1.addChallenge(challenge1);
+        user1.addChallenge(challenge4);
+        user1.addAcceptedChallenge(challenge1);
+        user1.addSession(session1);
+        user1.addSession(session4);
 
         User user2 = new User();
-        user2.setName("Jane Smith");
-        user2.setEmail("jane.smith@example.com");
-        user2.setBirthDate("1995-05-15");
-        user2.setProvider("Facebook");
-	
+        user2.addChallenge(challenge2);
+        user2.addChallenge(challenge1);
+        user2.addAcceptedChallenge(challenge2);
+        user2.addSession(session2);
+        user2.addSession(session1);
+
         User user3 = new User();
-        user3.setName("Alice Johnson");
-        user3.setEmail("alice.johnson@example.com");
-        user3.setBirthDate("1988-09-20");
-        user3.setProvider("Google");
+        user3.addChallenge(challenge3);
+        user3.addChallenge(challenge5);
+        user3.addAcceptedChallenge(challenge3);
+        user3.addSession(session3);
+        user3.addSession(session5);
 
         User user4 = new User();
-        user4.setName("Bob Anderson");
-        user4.setEmail("bob.anderson@example.com");
-        user4.setBirthDate("1992-12-08");
-        user4.setProvider("Facebook");
+        user4.addChallenge(challenge4);
+        user4.addChallenge(challenge2);
+        user4.addAcceptedChallenge(challenge4);
+        user4.addSession(session4);
+        user4.addSession(session2);
 
         User user5 = new User();
-        user5.setName("Eva Rodriguez");
-        user5.setEmail("eva.rodriguez@example.com");
-        user5.setBirthDate("1997-03-25");
-        user5.setProvider("Google");
-		
-		user1.addChallenge(challenge1);
-		user1.addChallenge(challenge4);
-		user1.addAcceptedChallenge(challenge1);
-		user1.addSession(session1);
-		user1.addSession(session4);
-		
-		user2.addChallenge(challenge2);
-		user2.addChallenge(challenge1);
-		user2.addAcceptedChallenge(challenge2);
-		user2.addSession(session2);
-		user2.addSession(session1);
-		
-		user3.addChallenge(challenge3);
-		user3.addChallenge(challenge5);
-		user3.addAcceptedChallenge(challenge3);
-		user3.addSession(session3);
-		user3.addSession(session5);
-		
-		user4.addChallenge(challenge4);
-		user4.addChallenge(challenge2);
-		user4.addAcceptedChallenge(challenge4);
-		user4.addSession(session4);
-		user4.addSession(session2);
-		
-		user5.addChallenge(challenge5);
-		user5.addChallenge(challenge3);
-		user5.addAcceptedChallenge(challenge5);
-		user5.addSession(session5);
-		user5.addSession(session3);
-		
-		registeredUsers.add(user1);
-		registeredUsers.add(user2);
-		registeredUsers.add(user3);
-		registeredUsers.add(user4);
-		registeredUsers.add(user5);
-		
-	}
-	
+        user5.addChallenge(challenge5);
+        user5.addChallenge(challenge3);
+        user5.addAcceptedChallenge(challenge5);
+        user5.addSession(session5);
+        user5.addSession(session3);
 
-	
-	// Mandatory arguments for registration
-	
-	public boolean register(String email, String name, String birthDate, String password, String provider) {
-		try {
-			User registerGoogleMandatory = new User(name, email, birthDate, provider);
-			
-			IProviderGateway externalService = GatewayFactory.getInstance().createGateway(provider);
-			boolean emailExists = externalService.validateEmail(email);
-			boolean registration = externalService.register(email, password);
-			if (!emailExists && registration) {
-				registeredUsers.add(registerGoogleMandatory);
-				return true;
-			} else {
-				return false;
-			}
-			
-	    } catch (Exception e) {e.printStackTrace(); return false;}
-	}
-	
-	// Mandatory + optional arguments for registration
-	
-	public boolean register(String email, String name, String birthDate, String password, String provider, float weight, int height, int maxHeartRate, int restHeartRate) {
-		try {
-			User registerGoogleMandatory = new User(name, email, birthDate, weight, height, maxHeartRate, restHeartRate, provider);
-			
-			IProviderGateway externalService = GatewayFactory.getInstance().createGateway(provider);
-			boolean emailExists = externalService.validateEmail(email);
-			boolean registration = externalService.register(email, password);
-			if (!emailExists && registration) {
-				registeredUsers.add(registerGoogleMandatory);
-				return true;
-			} else {
-				return false;
-			}
-			
-	    } catch (Exception e) {e.printStackTrace(); return false;}
-	}
-	
-	
-	public User login(String email, String password) {
-		
-		//Generate the hash of the password
-		//String hashPass = org.apache.commons.codec.digest.DigestUtils.sha1Hex("$!9PhNz,");	
-		//user1.setPassword(hashPass);
-		User user = new User();
-		for (User u : this.registeredUsers) {
-			if(u.getEmail().equals(email)) {
-				user.setName(u.getName());
-		        user.setEmail(u.getEmail());
-		        user.setBirthDate(u.getBirthDate());
-		        user.setSessionList(u.getSessionList());
-		        user.setAcceptedChallengeList(u.getAcceptedChallengeList());
-		        user.setChallengeList(u.getChallengeList());
-		        user.setProvider(u.getProvider());
-		        //user.setPassword(u.getPassword());
-				//String hashPass = org.apache.commons.codec.digest.DigestUtils.sha1Hex("$!9PhNz,");	
-				//user.setPassword(hashPass);
-			}
-		}
-		
-		IProviderGateway externalService = GatewayFactory.getInstance().createGateway(user.getProvider());
-		boolean validatePassword = externalService.validatePassword(email, password);
-		
-		//if (user.getEmail().equals(email) && user.checkPassword(org.apache.commons.codec.digest.DigestUtils.sha1Hex(password))) {		
-		if (user.getEmail().equals(email) && validatePassword) {	
-			return user;
-		} else {
-			return null;
-		}
-	}
+        registeredUsers.add(user1);
+        registeredUsers.add(user2);
+        registeredUsers.add(user3);
+        registeredUsers.add(user4);
+        registeredUsers.add(user5);
+    }
+
+    // Mandatory arguments for registration
+    public boolean register(String email, String name, String birthDate, String password, String provider) {
+        try {
+            User registerGoogleMandatory = new User(name, email, birthDate, provider);
+
+            IProviderGateway externalService = GatewayFactory.getInstance().createGateway(provider);
+            boolean emailExists = externalService.validateEmail(email);
+            boolean registration = externalService.register(email, password);
+
+            if (!emailExists && registration) {
+                registeredUsers.add(registerGoogleMandatory);
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    // Mandatory + optional arguments for registration
+    public boolean register(String email, String name, String birthDate, String password, String provider,
+                            float weight, int height, int maxHeartRate, int restHeartRate) {
+        try {
+            User registerGoogleMandatory = new User(name, email, birthDate, weight, height, maxHeartRate, restHeartRate, provider);
+
+            IProviderGateway externalService = GatewayFactory.getInstance().createGateway(provider);
+            boolean emailExists = externalService.validateEmail(email);
+            boolean registration = externalService.register(email, password);
+
+            if (!emailExists && registration) {
+                registeredUsers.add(registerGoogleMandatory);
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public User login(String email, String password) {
+        User user = new User();
+
+        for (User u : this.registeredUsers) {
+            if (u.getEmail().equals(email)) {
+                user.setName(u.getName());
+                user.setEmail(u.getEmail());
+                user.setBirthDate(u.getBirthDate());
+                user.setSessionList(u.getSessionList());
+                user.setAcceptedChallengeList(u.getAcceptedChallengeList());
+                user.setChallengeList(u.getChallengeList());
+                user.setProvider(u.getProvider());
+            }
+        }
+
+        IProviderGateway externalService = GatewayFactory.getInstance().createGateway(user.getProvider());
+        boolean validatePassword = externalService.validatePassword(email, password);
+
+        if (user.getEmail().equals(email) && validatePassword) {
+            return user;
+        } else {
+            return null;
+        }
+    }
 }
